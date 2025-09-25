8c9162faa45b16f0e998d20cff87c646950d7b62
gitindex <38837497+Gitindexjs@users.noreply.github.com>
2025-09-25 14:11:12 +0100

    Create math.cc

---
+++ math.cc
@@ -0,0 +1,40 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+
+std::vector<int> pUntil(int a) {
+    std::vector<int> p;
+    for(int i = 2; i < a; i++) {
+        for(int j = 0; j < p.size(); j++) {
+            if()
+        }
+    }
+}
+
+std::vector<int[2]> pFactors(int a) {
+    std::vector<int[2]> factors;
+    
+}
+
+bool isCoprime(int a, int b) {
+    
+}
+
+int inv(int a, int b) {
+    if(!isCoprime(a, b)) {
+        return -1;
+    }
+    int c = 1;
+    while((c*a)%b != 1){
+        c++;
+    }
+}
+
+int main() {
+    // Write C++ code here
+    std::cout << "Try programiz.pro";
+
+    return 0;
+}
