6b92d6ed1eca888f046125e4abcd31547f34aa4b
gitindex <38837497+Gitindexjs@users.noreply.github.com>
2025-09-26 17:18:28 +0100

    Update math.cc

--- math.cc
+++ math.cc
@@ -1,30 +1,98 @@
 // Online C++ compiler to run C++ program online
 #include <iostream>
 #include <vector>
+#include <array>
 #include <math.h>
 
-std::vector<int[2]> pFactors(int a) {
-    std::vector<int[2]> factors;
-    
+std::vector<int> pFactors(int a) {
+    std::vector<int> factors;
+    for(int i = 2; i < sqrt(a); i++) {
+        int seen = 0;
+        for(int j = 0; j < factors.size()/2; j++) {
+            if(i%factors[j*2]==0) {
+                seen++;
+            }
+        }
+        if(seen == 0) {
+            int c = 0;
+            while(a%i==0){
+                c++;
+                a/=i;
+            }
+            factors.push_back(i);
+            factors.push_back(c);
+        }
+    }
+    return factors;
 }
 
 bool isCoprime(int a, int b) {
+    std::vector<int> pA = pFactors(a);
+    std::vector<int> pB = pFactors(b);
     
+    int s = pA.size();
+    if(pB.size() < s) {
+        s = pB.size();
+    }
+    for(int i = 0; i < s/2; i++) {
+        if(!(pA[2*i+1] == 0 || pB[2*i+1] == 0)) {
+            return 0;
+        }
+    }
+    return 1;
 }
 
 int inv(int a, int b) {
-    if(!isCoprime(a, b)) {
-        return -1;
+    // erroneous flag
+    if(a == 0 || b == 0 || !isCoprime(a, b)) {
+        return 0;
+    }
+    b = abs(b);
+    while(a < 0) {
+        a += b;
     }
     int c = 1;
     while((c*a)%b != 1){
         c++;
     }
+    return c;
+}
+
+int avg(std::vector<int> a) {
+    int r = 0;
+    for(int i = 0; i < a.size(); i++){
+        r+= a[i];
+    }
+    return r/a.size();
+}
+
+int det(std::vector<std::vector<int>> b) {
+    int sum = 0;
+    int L = 0;
+    for(int i = 0; i < b.size(); i++){
+        if(b[i].size() != b.size()) {
+            return 0;
+        }
+        if(avg(b[L]) >avg(b[i])) {
+            L = i;
+        }
+    }
+    for(int i = 0; i < b[L].size(); i++){
+        // dubious
+        int c = b[L][i];
+        std::vector<std::vector<int>> s = b;
+        s.erase(s.begin() + L);
+        for(int j = 0; j < s.size(); j++) {
+            s[j].erase(s[j].begin() + i);
+        }
+        // pow?
+        sum += (-1)**i*c*det(s);
+    }
+    return sum;
 }
 
 int main() {
-    // Write C++ code here
-    std::cout << "Try programiz.pro";
+    std::cout << inv(-1, 2);
 
     return 0;
 }
