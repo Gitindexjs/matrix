50a1977088013d74cf294efa248198b92db9024c
gitindex <38837497+Gitindexjs@users.noreply.github.com>
2025-10-01 09:59:58 +0100

    Update math.cc

--- math.cc
+++ math.cc
@@ -117,17 +117,67 @@ std::vector<std::vector<int>> invM(std::vector<std::vector<int>> M, int b){
     std::vector<std::vector<int>> r;
     
     for(int i = 0; i < M.size(); i++){
-        r.push_back(std::vector<int>(M.size()));
+        r.push_back(std::vector<int>());
         for(int j = 0; j < M[i].size(); j++){
-            r[i].push_back(pow(-1, (i+j)%2) * det(reduceM(M, i, j)));
+            int cell = pow(-1, (i+j)%2) * det(reduceM(M, i, j)) * mP;
+            cell %= b;
+            while(cell < 0) {
+                cell += b;
+            }
+            r[i].push_back(cell);
         }
     }
-    
     return r;
 }
 
+std::vector<int> applyMatrix(std::vector<int> c, std::vector<std::vector<int>> M, int b) {
+    std::vector<int> r;
+    for(int i = 0; i < M.size(); i++) {
+        int sum = 0;
+        for(int j = 0; j < M[i].size(); j++) {
+            sum += M[i][j] * c[j];
+        }
+        while(sum < 0){
+            sum += b;
+        }
+        r.push_back(sum%b);
+    }
+    return r;
+}
+
+int B = 5;
+
 int main() {
-    std::vector<std::vector<int>> a = {{1,2},{3,4}};
-    std::cout << det(a);
+    // does it really work?
+    // sample tests with solving lights out then... assuming congruent data is fitted inside
+    std::vector<std::vector<int>> a;
+    for(int i = 0; i < pow(B, 2); i++) {
+        std::vector<int> r;
+        for(int j = 0; j < pow(B, 2); j++) {
+            int d = 0;
+            if(abs((j-i)%B) <= 1 || abs(j-i) == 5) {
+                d = 1;
+            }
+            r.push_back(d);
+        }
+        a.push_back(r);
+    }
+    std::vector<std::vector<int>> s = invM(a, 2);
+    std::vector<int> difference = {0, 1, 1, 1, 1,
+                                   1, 1, 0, 0, 1,
+                                   0, 0, 0, 0, 1,
+                                   0, 1, 0, 0, 0,
+                                   0, 1, 1, 0, 1
+    };
+    std::vector<int> solution = applyMatrix(difference, s, 2);
+    // for(int i = 0; i < s.size(); i++) {
+    //     for(int j = 0; j < s[i].size(); j++) {
+    //         std::cout << s[i][j] << " ";
+    //     }
+    //     std::cout << std::endl;
+    // }
+    for(int i = 0; i < solution.size(); i++) {
+        std::cout << solution[i] << " ";
+    }
     return 0;
 }
